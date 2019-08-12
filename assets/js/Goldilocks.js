b0736ee472c933cc5fc0fd39192e66f36aff82e2
Tom Howard <53379881+thowardii@users.noreply.github.com>
2019-08-12 01:54:43 -0400

    fix pics

--- assets/js/Goldilocks.js
+++ assets/js/Goldilocks.js
@@ -1,66 +1,69 @@
+
+
+
 function playGame() {
 
-var enterCabin = confirm("You come across a strange house in the woods. Do you want to go in?");
+  var enterCabin = confirm("You come across a strange house in the woods. Do you want to go in?");
 
-  if (enterCabin == true){
+  if (enterCabin == true) {
 
     var porridgeChoice = prompt("You walk into the cabin and see a lovely kitchen. On the table there are three bowls of porridge. Do you try 1, 2 or 3?");
-      
-      switch (porridgeChoice) {
-        
-        case "1":
+
+    switch (porridgeChoice) {
+
+      case "1":
         alert("The porridge is too hot! The End.");
         enterCabin = False;
         break;
-        
-        case "2":
+
+      case "2":
         alert("The porridge is too cold! The End.");
         enterCabin = False;
         break;
-        
-        case "3":
+
+      case "3":
         alert("This porridge is just right!");
-      }
-  
+    }
+
 
     if (enterCabin == true) {
-      
+
       var chairChoice = prompt("You walk into the living room and see three chairs.  A big one, medium on and small one.  Which do you choose?");
 
-        switch (chairChoice) {
-            
-            case "big":
-            alert("The big chair is way too big!  The End");
-            enterCabin = False
-            break;
-            
-            case "medium":
-            alert("The medium chair is too big.  The End");
-            enterCabin = False
-            break;
-            
-            case "small":
-            alert("This chair is just right...");
-        }
+      switch (chairChoice) {
+
+        case "big":
+          alert("The big chair is way too big!  The End");
+          enterCabin = False
+          break;
+
+        case "medium":
+          alert("The medium chair is too big.  The End");
+          enterCabin = False
+          break;
+
+        case "small":
+          alert("This chair is just right...");
+      }
     }
 
     if (enterCabin == true) {
-      
+
       var bedChoice = prompt("You walk into the next room over and see three beds, A big one, a medium one and a small one.  Which one do you choose?");
 
       switch (bedChoice) {
-          
-          case "big":
+
+        case "big":
           alert("It's too hard and you can't sleep.  The End!");
           breakenterCabin = False
           break;
-          
-          case "medium":
+
+        case "medium":
           alert("It's too soft and you can't sleep.  The End!");
           enterCabin = False
           break;
-          
-          case "small":
+
+        case "small":
           alert("It's just right and you fall into a deep slumber.");
           alert("You wake up to see three bears looking at you.");
           alert("Shocked you get up and run away never to return again.");
@@ -75,8 +78,8 @@ var enterCabin = confirm("You come across a strange house in the woods. Do you w
 
 var playAgain = confirm("Do you want to play again");
 
-  if (playAgain == true) {
-    playGame()
-  }
-
+if (playAgain == true) {
   playGame()
+}
+
+playGame()
